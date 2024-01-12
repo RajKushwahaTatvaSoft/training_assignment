262af12edd50799c0b462554ff0f0d89d7003f57
Raj Kushwaha <PCA195@server7.com>
2024-01-12 18:16:58 +0530

    added innerpage and made changes for responsive design

--- js/custom.js
+++ js/custom.js
@@ -1,1 +1,29 @@
-// JavaScript source code
+var flag = true;
+
+function toggleSidebar() {
+
+    var sidebar = document.getElementById('sidebar')
+    var main = document.getElementById('main-content')
+    if (flag) {
+
+        sidebar.classList.remove('d-inline')
+        sidebar.classList.add('d-none')
+
+        sidebar.classList.remove('col-md-2')
+        main.classList.remove('col-md-10')
+        main.classList.add('col-md-12')
+        
+        flag = !flag;
+    }
+    else {
+
+        sidebar.classList.remove('d-none')
+        sidebar.classList.add('d-inline')
+
+        sidebar.classList.add('col-md-2')
+        main.classList.add('col-md-10')
+        main.classList.remove('col-md-12')
+
+        flag = !flag;
+    }
+}
